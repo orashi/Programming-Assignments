4025b8a087da258c2454259e13a43cffba49326d
Yuanzheng Ci <orashi@mail.dlut.edu.cn>
2016-02-07 17:10:00 +0800

    sum 递归改迭代

--- SICP/1.29 Simpson-rule.rkt
+++ SICP/1.29 Simpson-rule.rkt
@@ -1,9 +1,10 @@
 #lang planet neil/sicp
 (define (sum term a next b)
-  (if (> a b)
-      0
-      (+ (term a)
-         (sum term (next a) next b))))
+  (define(iter a result)
+    (if (> a b)
+        result
+        (iter (next a)(+ result (term a)))))
+  (iter a 0))
 (define (Simpson-rule f a b n)
   (define h (/(- b a)
               n))
